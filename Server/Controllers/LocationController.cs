89cd6c081e791c5d094d4160a4f2bc3d5d3d823a
Chuma M'hango <chumamhango04@gmail.com>
2023-08-02 19:15:29 +0200

    This commit has insignificant changes. The previous one, however, includes code for determining that the location if in a restricted area.

--- Server/Controllers/LocationController.cs
+++ Server/Controllers/LocationController.cs
@@ -1,7 +1,5 @@
 ﻿global using DynamicSpeedControl.Shared.Models;
 
-using Location = DynamicSpeedControl.Server.Models.LocationObject;
-
 namespace DynamicSpeedControl.Server.Controllers
 {
     
@@ -14,14 +12,12 @@ namespace DynamicSpeedControl.Server.Controllers
         {
             // get all restricted areas from the database
             RestrictedAreas = _dataContext.RestrictedAreas.ToList();
-        }
-
-        
+        }  
 
         [HttpPost]
-        public ActionResult<RestrictedAreaEntity> CheckLocation(Location location) 
+        public ActionResult<RestrictedAreaEntity> CheckLocation(Coordinate currentLocation) 
             => Ok(_dataContext.RestrictedAreas.FirstOrDefault(restrictedArea 
-                => restrictedArea.IsInArea(location)));
+                => restrictedArea.IsInArea(currentLocation)));
 
     }
 }
