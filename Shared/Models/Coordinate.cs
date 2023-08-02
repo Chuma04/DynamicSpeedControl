e9d93e1fa03336737989229f827a192a57050b0f
Chuma M'hango <chumamhango04@gmail.com>
2023-08-02 19:13:47 +0200

    Revert "Revert "Updated location-Coordinates details""
    This reverts commit 3f06ac48ee2937fc9984d73645f6a5fea5c83573.

---
+++ Shared/Models/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSpeedControl.Shared.Models
+{
+    public class Coordinate : BaseEntity
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- Shared/Models/RestrictedAreaEntity.cs
+++ Shared/Models/RestrictedAreaEntity.cs
@@ -1,18 +1,34 @@
-﻿using DynamicSpeedControl.Server.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DynamicSpeedControl.Shared.Models
+﻿namespace DynamicSpeedControl.Shared.Models
 {
     public class RestrictedAreaEntity : BaseEntity
     {
+        public List<Coordinate> Perimeter { get; set; }
 
-        public bool IsInArea(LocationObject location)
+        // Method to check if the current location is in the restricted area
+        public bool IsInArea(Coordinate currentLocation)
         {
-            throw new NotImplementedException();
+            // get all coordinates of the restricted area
+            Coordinate[] areaCoordinates = this.Perimeter.ToArray();
+
+            bool inside = false;
+            int j = areaCoordinates.Length - 1; // index of the last vertex
+
+            for (int i = 0; i < areaCoordinates.Length; j = i++) // loop through all vertices of the HomeCoordinates
+            {
+                // check if the point is on an horizontal edge of the areaCoordinates
+                if (areaCoordinates[i].Longitude == currentLocation.Longitude && areaCoordinates[j].Longitude == currentLocation.Longitude &&
+                    (areaCoordinates[i].Latitude >= currentLocation.Latitude) != (areaCoordinates[j].Latitude >= currentLocation.Latitude))
+                    return true;
+                // check if the edge intersects with the ray
+                if ((areaCoordinates[i].Longitude > currentLocation.Longitude) 
+                    != (areaCoordinates[j].Longitude > currentLocation.Longitude) 
+                    && currentLocation.Latitude < (areaCoordinates[j].Latitude
+                        - areaCoordinates[i].Latitude) * (currentLocation.Longitude
+                        - areaCoordinates[i].Longitude) / (areaCoordinates[j].Longitude
+                        - areaCoordinates[i].Longitude) + areaCoordinates[i].Latitude)
+                    inside = !inside; // toggle state
+            }
+            return inside;
         }
     }
 }
