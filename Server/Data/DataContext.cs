6fd85420a85fd7854632852866b7009c40755a7c
Chuma M'hango <chumamhango04@gmail.com>
2023-08-02 13:15:13 +0200

    DataContext

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DynamicSpeedControl.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<RestrictedAreaEntity> RestrictedAreas { get; set; }
+    }
+}
